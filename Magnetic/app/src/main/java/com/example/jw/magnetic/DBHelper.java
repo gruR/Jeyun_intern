52f167033800fe5140d701ab15e78f923de26ebe
jaewoon2 <jjaewoon2@gmail.com>
2019-01-11 15:05:39 +0900

    디비 수정

--- Magnetic/app/src/main/java/com/example/jw/magnetic/DBHelper.java
+++ Magnetic/app/src/main/java/com/example/jw/magnetic/DBHelper.java
@@ -7,19 +7,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     public DBHelper(Context context){
-        super(context, "magnetic.db", null, 1);
+        super(context, "navi.db", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE mValue "+
                 "(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "magX REAL, magY REAL, magZ REAL);");
+                "magX REAL, magY REAL, magZ REAL, magT REAL);");
+        db.execSQL("CREATE TABLE wValue "+
+                "(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "macId TEXT, ssId TEXT, wifi REAL);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS mValue;");
+        db.execSQL("DROP TABLE IF EXISTS wValue;");
         onCreate(db);
     }
 }
