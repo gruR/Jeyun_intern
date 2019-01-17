dabbc7f751e4cdf40b3a26e40aecf2c2b05b115d
ShinHoKyung <parkland106e@naver.com>
2019-01-17 14:20:16 +0900

    Main Wifi

--- main_map/app/src/main/java/com/example/jeyun/main_map/MainActivity.java
+++ main_map/app/src/main/java/com/example/jeyun/main_map/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.jeyun.main_map;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+        WifiManager wifiManager;
+        List<ScanResult> mScanResult;
         //MODE
         public enum MODE_STATE {
             Calculate, Calibration, Initialization, WaitingMove, DR
@@ -26,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+            if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
+            }
 
             in_mag_arr = new double[110];
             pos_arr = new int[3];
@@ -34,9 +52,61 @@ public class MainActivity extends AppCompatActivity {
 
             q_mag_x = new float[QSIZE];
             q_mag_z = new float[QSIZE];
+            wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            if(wifiManager!=null)
+            {
+                if(!wifiManager.isWifiEnabled())
+                {
+                    wifiManager.setWifiEnabled(true);
+                }
+                final IntentFilter filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+                filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+                registerReceiver(WifiScanReceiver,filter);
+                wifiManager.startScan();
+            }
         }
+        private BroadcastReceiver WifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                final String action = intent.getAction();
+                if(action!=null)
+                {
+                    if(action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+                    {
+                        mScanResult = wifiManager.getScanResults();
+                        wifiManager.startScan();
+                    }
+                    else if(action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION))
+                    {
+                        context.sendBroadcast(new Intent("wifi.ON_NETWORK_STATE_CHANGED"));
+                    }
+                }
+            }
+        };
 
-        @Override
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(WifiScanReceiver);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(WifiScanReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final IntentFilter filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        registerReceiver(WifiScanReceiver,filter);
+        wifiManager.startScan();
+    }
+
+    @Override
         public void onBackPressed() {
             mapViewFragment.moveMap.clearExpectedCircle();
             AlertDialog.Builder alert_exit = new AlertDialog.Builder(this);
@@ -73,5 +143,4 @@ public class MainActivity extends AppCompatActivity {
         mapViewFragment.changeDirecton((4 - direction_frombt) * 90 + degree);
         direction_frombt = degree/90;
     }*/
-
 }
